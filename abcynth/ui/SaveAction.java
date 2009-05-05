55c6f6231c791df704e260a472c7a0d59b4c0e20
pipemakertjm <pipemakertjm@60e0f196-961e-0410-a92f-9d946f01d26a>
2009-05-05 15:00:03 +0000

    Integration with chord names, slurs and ties work.

---
+++ abcynth/ui/SaveAction.java
@@ -0,0 +1,41 @@
+// Copyright 2006-2008 Lionel Gueganton
+// This file is part of abc4j.
+//
+// abc4j is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// abc4j is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with abc4j.  If not, see <http://www.gnu.org/licenses/>.
+package abcynth.ui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.io.IOException;
+
+public class SaveAction extends TuneBookActionAbstract
+{
+
+  private static final long serialVersionUID = -6714276449739610799L;
+
+  public SaveAction()
+  {
+    putValue(NAME, "Save");
+    putValue(SHORT_DESCRIPTION, "Saves tunebook updates");
+    putValue(MNEMONIC_KEY, new Integer(KeyEvent.VK_S));
+  }
+
+  public void actionPerformed(ActionEvent e)
+  {
+    try
+    { getTuneBook().save(); }
+    catch(IOException ex)
+    { ex.printStackTrace(); }
+  }
+}
